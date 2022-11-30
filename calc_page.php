6689c56f4edc71aa19e62ab2ae4fb0517b701077
paperStudent <newso.dartray@gmail.com>
2022-11-30 17:33:09 +0900

    [update]

--- calc_page.php
+++ calc_page.php
@@ -1,62 +1,73 @@
-<?php include('_header.php')?>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-<script src="/staff/js/include_calc.js"></script>
-
 <?php
-	$check_number	= isset($_GET["nbox"]) ? $_GET["nbox"] : 0;
-	$answer		= calculation($check_number);
-	$len			= count($answer);
 
-	function calculation($t){
+include('_header.php');
 
-		$answer_ary = array();
+define('MAX_NUM',100000);
 
-		if($t < 2){
-			return $answer_ary;
-		}
+$check_number	= 0;
 
-		$answer_ary[] = 2;
+if(isset($_GET["nbox"])){
+	if(($_GET["nbox"]) > MAX_NUM){
+		$check_number = 0;
+	}else{
+		$check_number = $_GET["nbox"];
+	}
+}
 
-		for ($i=2; $i<=$t; $i++) {
-			if($i % 2 === 0){
-				continue;
-			}
-			$odd = $i;
-			$odd_ary[] = $odd;
-		}
-		
-		$odd_len = count($odd_ary);
+$answer		= calculation($check_number);
+$len			= count($answer);
 
-		for($i=0; $i<$odd_len; $i++){
+function calculation($t){
 
-			$not_prime_num_flg = 0;
+	$answer_ary = array();
 
-			if($i === 0 || $i === 1){
-				$answer_ary[] = $odd_ary[$i];
-				continue;
-			}
-
-			$target_odd = $odd_ary[$i];
-
-			for($j=0; $j<$i-1; $j++){
-				$res = $target_odd % $odd_ary[$j];
-				if($res === 0){
-					$not_prime_num_flg = 1;
-					break;
-				}
-			}
-
-			if($not_prime_num_flg){
-				continue;
-			}
-
-			$answer_ary[] = $target_odd;
-		}
+	if($t < 2){
 		return $answer_ary;
 	}
+
+	$answer_ary[] = 2;
+
+	for ($i=2; $i<=$t; $i++) {
+		if($i % 2 === 0){
+			continue;
+		}
+		$odd = $i;
+		$odd_ary[] = $odd;
+	}
+	
+	$odd_len = count($odd_ary);
+
+	for($i=0; $i<$odd_len; $i++){
+
+		$not_prime_num_flg = 0;
+
+		if($i === 0 || $i === 1){
+			$answer_ary[] = $odd_ary[$i];
+			continue;
+		}
+
+		$target_odd = $odd_ary[$i];
+
+		for($j=0; $j<$i-1; $j++){
+			$res = $target_odd % $odd_ary[$j];
+			if($res === 0){
+				$not_prime_num_flg = 1;
+				break;
+			}
+		}
+
+		if($not_prime_num_flg){
+			continue;
+		}
+
+		$answer_ary[] = $target_odd;
+	}
+	return $answer_ary;
+}
+
+include('tmpl/calc_page.html');
+include('_footer.php');
 ?>
 
-<?php include('tmpl/calc_page.html')?>
 
-<?php include('_footer.php')?>
+
--- js/include_calc.js
+++ js/include_calc.js
@@ -1,13 +1,14 @@
 
 $(function(){
 
+	$('.target-ball').fadeIn(300);
+
 	$('button').on('click', function(){
 		let val = $('input[name=nbox]').val() === null? 0 : $('input[name=nbox]').val();
 		if(parseInt(val) > parseInt($('input[name=nbox]').attr('max'))){
 			maxNumAlert();
 		}
 		$('form').submit();
-		return false;
 	});
 
 	$('input[name=nbox]').change(function(){
@@ -15,13 +16,12 @@ $(function(){
 		if(parseInt(val) > parseInt($(this).attr('max'))){
 			maxNumAlert();
 		}
-		return false;
 	});
-	$('.target-ball').fadeIn(300);
 });
 
 function maxNumAlert(){
-	alert('数値は100000を超えないように設定してください。');
+	let max_num = $('input[name=nbox]').attr('max');
+	alert('数値は'+ max_num +'を超えないように設定してください。');
 	$('input[name=nbox]').val(null);
 	return false;
 }
